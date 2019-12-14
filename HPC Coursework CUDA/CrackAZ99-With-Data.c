9fed7c2b10d84f7d2fad916b7f06aabeff255395
bidur20540509 <58352577+bidur20540509@users.noreply.github.com>
2019-12-14 12:36:38 +0545

    Add files via upload

---
+++ HPC Coursework CUDA/CrackAZ99-With-Data.c
@@ -0,0 +1,161 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <crypt.h>
+#include <time.h>
+#include <pthread.h>
+
+/***********************************************************************
+*******
+  Demonstrates how to crack an encrypted password using a simple
+  "brute force" algorithm. Works on passwords that consist only of 2
+uppercase
+  letters and a 2 digit integer. Your personalised data set is included
+in the
+  code.
+
+  Compile with:
+    cc -o CrackAZ99-With-Data CrackAZ99-With-Data.c -lcrypt-pthread
+
+  If you want to analyse the results then use the redirection operator
+to send
+  output to a file that you can view using an editor or the less
+utility:
+
+    ./CrackAZ99-With-Data > results.txt
+
+  Dr Kevan Buckley, University of Wolverhampton, 2018
+************************************************************************
+******/
+int n_passwords = 4;
+
+char *encrypted_passwords[] = {
+
+"$6$KB$6SsUGf4Cq7/Oooym9WWQN3VKeo2lynKV9gXVyEG4HvYy1UFRx.XAye89TLp/OTcW7cGpf9UlU0F.cK/S9CfZn1",
+
+"$6$KB$kwebHrK29hjigYG8zvp87N2VpoQE51HML2nZsPyUYIRTcdOpbfCWgjr5wfdXEgNJFOMyt1weZuyTr8AmTZZ8d/",
+
+"$6$KB$OtjvdN6IOVby7U6PRYd2fBEjTv6R0IpsFfsy5JwtyZfsz5rBdrMnARYV9s7PxHkvKTI8wLz5YbEYFCFq1SUgC1",
+
+"$6$KB$MD4fomhxal9x9Bc5K06IHDh32W.8osCGJepMkj8H1QRuHP9OAtR8UpGHEGZ4Rj0mOm/qIBhnriusixnF6rpPW1"
+};
+
+/**
+ Required by lack of standard function in C.   
+*/
+
+void substr(char *dest, char *src, int start, int length){
+  memcpy(dest, src + start, length);
+  *(dest + length) = '\0';
+}
+
+/**
+ This function can crack the kind of password explained above. All
+combinations
+ that are tried are displayed and when the password is found, #, is put
+at the
+ start of the line. Note that one of the most time consuming operations
+that
+ it performs is the output of intermediate results, so performance
+experiments
+ for this kind of program should not include this. i.e. comment out the
+printfs.
+*/
+void threadpass()
+{
+int i;
+pthread_t b1, b2;
+	void *kernel_function_1();
+	void *kernel_function_2();
+for(i=0;i<n_passwords;i<i++){
+
+	pthread_create(&b1, NULL ,kernel_function_1, encrypted_passwords[i]);
+	pthread_create(&b2, NULL ,kernel_function_2, encrypted_passwords[i]);
+
+	pthread_join(b1, NULL);
+	pthread_join(b2, NULL);
+	}
+}
+void *kernel_function_1(char *salt_and_encrypted){
+  int b, d, r;     // Loop counters
+  char salt[7];    // String used in hahttps://www.youtube.com/watch?v=L8yJjIGleMwshing the password. Need space
+  char plain[7];   // The combination of letters currently being checked
+  char *enc;       // Pointer to the encrypted password
+  int count = 0;   // The number of combinations explored so far
+
+  substr(salt, salt_and_encrypted, 0, 6);
+
+  for(b='A'; b<='Z'; b++){
+    for(d='A'; d<='Z'; d++){
+      for(r=0; r<=99; r++){
+        sprintf(plain, "%c%c%02d", b, d, r);
+        enc = (char *) crypt(plain, salt);
+        count++;
+        if(strcmp(salt_and_encrypted, enc) == 0){
+          printf("#%-8d%s %s\n", count, plain, enc);
+        } else {
+          printf(" %-8d%s %s\n", count, plain, enc);
+        }
+      }
+    }
+  }
+  printf("%d solutions explored\n", count);
+}
+void *kernel_function_2(char *salt_and_encrypted){
+  int x, y, z;     // Loop counters
+  char salt[7];    // String used in hahttps://www.youtube.com/watch?v=L8yJjIGleMwshing the password. Need space
+  char plain[7];   // The combination of letters currently being checked
+  char *enc;       // Pointer to the encrypted password
+  int count = 0;   // The number of combinations explored so far
+
+  substr(salt, salt_and_encrypted, 0, 6);
+
+  for(x='A'; x<='Z'; x++){
+    for(y='A'; y<='Z'; y++){
+      for(z=0; z<=99; z++){
+        sprintf(plain, "%c%c%02d", x, y, z);
+        enc = (char *) crypt(plain, salt);
+        count++;
+        if(strcmp(salt_and_encrypted, enc) == 0){
+          printf("#%-8d%s %s\n", count, plain, enc);
+        } else {
+          printf(" %-8d%s %s\n", count, plain, enc);
+        }
+      }
+    }
+  }
+  printf("%d solutions explored\n", count);
+}
+
+
+//Calculating time
+
+int time_difference(struct timespec *start, struct timespec *finish, long long int *difference)
+ {
+	  long long int ds =  finish->tv_sec - start->tv_sec; 
+	  long long int dn =  finish->tv_nsec - start->tv_nsec; 
+
+	  if(dn < 0 ) {
+	    ds--;
+	    dn += 1000000000; 
+  } 
+	  *difference = ds * 1000000000 + dn;
+	  return !(*difference > 0);
+}
+int main(int argc, char *argv[])
+{
+  	int i;
+	struct timespec start, finish;   
+  	long long int time_elapsed;
+
+  	clock_gettime(CLOCK_MONOTONIC, &start);
+
+  	
+    		threadpass();
+  	
+	clock_gettime(CLOCK_MONOTONIC, &finish);
+	  time_difference(&start, &finish, &time_elapsed);
+	  printf("Time elapsed was %lldns or %0.9lfs\n", time_elapsed,
+		                                 (time_elapsed/1.0e9)); 
+  return 0;
+}
